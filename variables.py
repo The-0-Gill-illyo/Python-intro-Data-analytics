9f5e922961de1fe7e34b74a2d3658e2dbb3ddf4b
The-0-Gill-illyo <theogill1369@gmail.com>
2023-02-01 19:22:04 -0500

    included committed out instructions

--- variables.py
+++ variables.py
@@ -1,4 +1,10 @@
 
+# Task 1: Age
+# Declare a variable and store your age in that variable. 
+# Use string interpolation to format a message to the console
+# utilizing the previously declared variable. Print to the console
+# “I am XX years old”, where XX represents the value of your age.
+# You cannot *hard-code the value of your age in the string.
 
 my_age = 35
 txt = "I am {} years old"
